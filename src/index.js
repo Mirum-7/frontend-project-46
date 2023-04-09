f27b9f13ba667273c9fe6770ea3da2b03a76e30f
Эдуард <mirum7@icloud.com>
2023-04-09 20:19:59 +0300

    add "--format" option

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ export const startGenDiffCommand = () => {
     .name('genDiff')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0');
+  
+  program
+    .option('-f, --format <type>', 'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>');
 
   program.parse();
 }

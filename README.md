318e6eeccbcb8027280114bee4bc69fe0ddab789
Эдуард <mirum7@icloud.com>
2023-04-13 03:06:20 +0300

    add test coverage action

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### Tests and linter status:
 [![Actions Status](https://github.com/Mirum-7/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/Mirum-7/frontend-project-46/actions)
-[![Actions Status](https://github.com/Mirum-7/frontend-project-46/workflows/run-jest/badge.svg)](https://github.com/Mirum-7/frontend-project-46/actions)
-[![Actions Status](https://github.com/Mirum-7/frontend-project-46/workflows/run-lint/badge.svg)](https://github.com/Mirum-7/frontend-project-46/actions)
+[![Test and linter status](https://github.com/Mirum-7/frontend-project-46/workflows/run-test/badge.svg)](https://github.com/Mirum-7/frontend-project-46/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/4af4ae66daffa7e5f160/maintainability)](https://codeclimate.com/github/Mirum-7/frontend-project-46/maintainability)
---
+++ __tests__/index.test.js
@@ -0,0 +1,64 @@
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { readFrom, getUnionKeys } from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+let relativePath;
+let fileName;
+let fullPath;
+
+let obj1;
+let obj2;
+
+beforeAll(() => {
+  relativePath = '../__fixtures__/';
+  fileName = 'file.json';
+  fullPath = path.join(__dirname, relativePath, fileName);
+});
+
+beforeEach(() => {
+  obj1 = {
+    1: 'line 1',
+    2: 'line 2',
+    line1: 'some text',
+    line2: 'text',
+  };
+
+  obj2 = {
+    1: 'line 1',
+    3: 'line 3',
+    line1: 'too some text',
+    line2: 'text',
+    line3: 'some text',
+  };
+});
+
+describe('work with file', () => {
+  test('Read file', () => {
+    const content = readFrom(fullPath);
+
+    expect(content).toEqual(`{
+  "host": "hexlet.io",
+  "timeout": 50,
+  "proxy": "123.234.53.22",
+  "follow": false
+}`);
+  });
+});
+
+describe('work with object', () => {
+  test('get sorted union of keys', () => {
+    expect(getUnionKeys(obj1, obj2)).toEqual(['1', '2', '3', 'line1', 'line2', 'line3']);
+
+    obj2 = [];
+
+    expect(getUnionKeys(obj1, obj2)).toEqual(['1', '2', 'line1', 'line2']); // один элемент пустой
+
+    obj1 = [];
+
+    expect(getUnionKeys(obj1, obj2)).toEqual([]); // все пустые
+  });
+});

0f49f9480b3c52f2b1f03beead56980e916523c5
Эдуард <mirum7@icloud.com>
2023-05-18 19:02:30 +0500

    add const DEFAULT_SEP

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,34 +1,35 @@
 import _ from 'lodash';
 
-export const stringify = (obj, sep = '    ', deep = 0) => {
+const DEFAULT_SEP = '    ';
+
+export const stringify = (obj, deep = 0) => {
   if (!_.isObject(obj)) {
     return `${obj}`;
   }
 
-  const stringifiedElements = Object.entries(obj).map(([key, value]) => `${sep.repeat(deep + 1)}${key}: ${stringify(value, sep, deep + 1)}`).join('\n');
+  const stringifiedElements = Object.entries(obj).map(([key, value]) => `${DEFAULT_SEP.repeat(deep + 1)}${key}: ${stringify(value, deep + 1)}`).join('\n');
 
-  return `{\n${stringifiedElements}\n${sep.repeat(deep)}}`;
+  return `{\n${stringifiedElements}\n${DEFAULT_SEP.repeat(deep)}}`;
 };
 
 const stylish = (tree) => {
-  const sep = '    ';
-  const f1sep = '  - ';
-  const f2sep = '  + ';
+  const deletedSep = '  - ';
+  const addedSep = '  + ';
 
   const iter = (list, deep) => {
     const text = list
       .flatMap((el) => {
         switch (el.type) {
           case 'tree':
-            return `${sep.repeat(deep)}${el.key}: {\n${iter(el.children, deep + 1)}\n${sep.repeat(deep)}}`;
+            return `${DEFAULT_SEP.repeat(deep)}${el.key}: {\n${iter(el.children, deep + 1)}\n${DEFAULT_SEP.repeat(deep)}}`;
           case 'equal':
-            return `${sep.repeat(deep)}${el.key}: ${el.value}`;
+            return `${DEFAULT_SEP.repeat(deep)}${el.key}: ${el.value}`;
           case 'added':
-            return `${sep.repeat(deep - 1)}${f2sep}${el.key}: ${stringify(el.value2, sep, deep)}`;
+            return `${DEFAULT_SEP.repeat(deep - 1)}${addedSep}${el.key}: ${stringify(el.value2, deep)}`;
           case 'deleted':
-            return `${sep.repeat(deep - 1)}${f1sep}${el.key}: ${stringify(el.value1, sep, deep)}`;
+            return `${DEFAULT_SEP.repeat(deep - 1)}${deletedSep}${el.key}: ${stringify(el.value1, deep)}`;
           case 'changed':
-            return `${sep.repeat(deep - 1)}${f1sep}${el.key}: ${stringify(el.value1, sep, deep)}\n${sep.repeat(deep - 1)}${f2sep}${el.key}: ${stringify(el.value2, sep, deep)}`;
+            return `${DEFAULT_SEP.repeat(deep - 1)}${deletedSep}${el.key}: ${stringify(el.value1, deep)}\n${DEFAULT_SEP.repeat(deep - 1)}${addedSep}${el.key}: ${stringify(el.value2, deep)}`;
           default:
             throw new Error(`unknown element type: ${el.type}`);
         }
